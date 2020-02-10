0327f226df8e430652461764c1f9b4da2f8f3da0
ratsclub <ratsclub@protonmail.ch>
2020-02-10 19:42:35 +0000

    implements the clear command

---
+++ app/cogs/mod/clear.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+class Clear(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, number):
+        '''Bulk deletes messages from a channel'''
+        try:
+            number = int(number)
+            
+            if number < 0 or number > 99:
+                await ctx.send("You must provide a number between 1-99")
+                return
+            
+            msgs = []
+            async for x in ctx.channel.history(limit=number+1):
+                msgs.append(x)
+
+            await ctx.channel.delete_messages(msgs)
+        except ValueError as e:
+            await ctx.send("You must provide a integer number")
+            
+def setup(client):
+    client.add_cog(Clear(client))
